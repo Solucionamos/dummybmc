24d4fee92c1c2ff4bac1fe09d9b436748234a48c
Guilherme Marques <guiocm@gmail.com>
2014-12-01 23:16:53 -0200

    Add argument for execution of defective server.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 
 import sys
 import argparse
-from server import xml_server
+from server import xml_server, defective_servers
 from connection import http_connection
 
 parser = argparse.ArgumentParser()
@@ -16,8 +16,12 @@ parser.add_argument('-p', '--port', type=int, default=8080,
         help="server's HTTP port")
 parser.add_argument('--sensordata', type=str,
         default='server/sensor_data.csv', help="sensor data file")
+parser.add_argument('--randomloss', action='store_true')
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    server = xml_server.XMLServer(args.sensordata)
+    if args.randomloss:
+        server = defective_servers.RandomLossXMLServer(args.sensordata)
+    else:
+        server = xml_server.XMLServer(args.sensordata)
     connection = http_connection.HttpConnection(server, port=args.port)
